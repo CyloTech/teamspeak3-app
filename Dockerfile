19fec551cf4743e5be37953acf5625cc6612f81e
Glow <gavin@cylo.io>
2018-12-19 18:07:44 +0000

    Working.

--- Dockerfile
+++ Dockerfile
@@ -10,18 +10,20 @@ ENV QUERY_PORT 10002
 ENV FILE_PORT 10003
 
 RUN apt-get update \
-    && DEBIAN_FRONTEND=noninteractive apt-get -y install bzip2 wget ca-certificates \
+    && DEBIAN_FRONTEND=noninteractive apt-get -y install bzip2 wget ca-certificates netcat \
     && rm -rf /var/lib/apt/lists/* \
     && useradd -M -s /bin/false --uid 1000 teamspeak3 \
     && mkdir /data \
-    && chown teamspeak3:teamspeak3 /data
+    && chown teamspeak3:teamspeak3 /data \
+    && mkdir /app \
+    && chown teamspeak3:teamspeak3 /app
 
 COPY start-teamspeak3.sh /start-teamspeak3
 
 #EXPOSE 9987/udp 10011 30033
 
-#USER teamspeak3
-#VOLUME /data
-#WORKDIR /data
+USER teamspeak3
+VOLUME /data
+WORKDIR /data
 CMD ["/start-teamspeak3"]
 
--- start-teamspeak3.sh
+++ start-teamspeak3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -x
 
-if [ ! -f /etc/ts_installed ]; then
+export LD_LIBRARY_PATH=".:/data:/data/redist"
+
+if [ ! -f /app/ts_installed ]; then
     cd /data
 
     TARFILE=teamspeak3-server_linux_amd64-${TS_VERSION}.tar.bz2
@@ -24,8 +25,6 @@ if [ ! -f /etc/ts_installed ]; then
       && echo $TS_VERSION >version
     fi
 
-    export LD_LIBRARY_PATH=".:/data:/data/redist"
-
     TS3ARGS=""
     if [ -e /data/ts3server.ini ]; then
       TS3ARGS="inifile=/data/ts3server.ini"
@@ -38,9 +37,30 @@ if [ ! -f /etc/ts_installed ]; then
     fi
 
     touch /data/.ts3server_license_accepted
-    touch /etc/ts_installed
-fi
+
+    touch /data/tsoutput
+    ./ts3server license_accepted=1 query_port=${QUERY_PORT} default_voice_port=${VOICE_PORT} filetransfer_port=${FILE_PORT} $TS3ARGS > /data/tsoutput &
+
+    while ! nc -z localhost ${QUERY_PORT}; do
+      echo "Sleeping for 1 second whilst we wait for it to come online..."
+      sleep 1
+    done
+
+    TOKEN=$(cat /data/tsoutput | grep token= | awk '{print $5}')
+    TOKEN=$(echo "${TOKEN/|token=/}")
+
+    echo "*******************************************************************"
+    echo "The Token is ${TOKEN}"
+    echo "*******************************************************************"
+
+    tail -f /dev/null
+
+    touch /app/ts_installed
+    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/$INSTANCE_ID"
+
+else
 
 cd /data
 exec ./ts3server license_accepted=1 query_port=${QUERY_PORT} default_voice_port=${VOICE_PORT} filetransfer_port=${FILE_PORT} $TS3ARGS
 
+fi
